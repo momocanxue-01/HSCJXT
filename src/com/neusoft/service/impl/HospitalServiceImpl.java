a9cbe1c7ae932d379ad9dd982f75664ef89f68c0
momocanxue <lwp@qq.com>
2021-04-06 19:54:34 +0800

    核酸检测项目第四次提交 1.实现了动态管理医院 2.添加了管理员能删除医院的功能

---
+++ src/com/neusoft/service/impl/HospitalServiceImpl.java
@@ -0,0 +1,27 @@
+package com.neusoft.service.impl;
+
+import com.neusoft.mapper.HospitalMapper;
+import com.neusoft.model.Hospital;
+import com.neusoft.service.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HospitalServiceImpl implements HospitalService {
+
+    @Autowired
+    HospitalMapper hospitalMapper;
+
+    @Override
+    public List<Hospital> selectHospital() {
+        //调用mapper中的全查询方法
+        return hospitalMapper.selectHospital();
+    }
+
+    @Override
+    public int deleteHospital(int id) {
+        return hospitalMapper.deleteHospital(id);
+    }
+}
